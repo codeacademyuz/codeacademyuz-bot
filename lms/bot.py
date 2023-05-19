5129a5675c532004d346f6ef6c6a2a6a9d246cc5
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-19 15:45:09 +0500

    welcome msg

--- lms/bot.py
+++ lms/bot.py
@@ -1,7 +1,16 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update
 
+welcome_text = """
+Assalomu alaykum, {first_name}\!
+
+*CODEACADEMY* dasturchilar maktabi botiga xush kelibsiz\!
+
+Kelejak kasbini o'rganmoqchi bo'lsangiz bizga qo'shiling\!
+
+*📱 Telegram:* @codeacademyai
+*📞 Telefon:* \+998 95 012 77 33
+"""
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Hello!')
-
+    update.message.reply_markdown_v2(welcome_text.format(first_name=update.effective_user.first_name))
