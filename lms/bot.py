ac7904e3da731a07a8ecb3bf6b92203d426fbb36
JavohirJalilov <javohirjaliloff@gmail.com>
2023-05-21 18:01:05 +0500

    message constant

--- lms/bot.py
+++ lms/bot.py
@@ -1,16 +1,18 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update
-
-welcome_text = """
-Assalomu alaykum, {first_name}\!
-
-*CODEACADEMY* dasturchilar maktabi botiga xush kelibsiz\!
-
-Kelejak kasbini o'rganmoqchi bo'lsangiz bizga qo'shiling\!
-
-*📱 Telegram:* @codeacademyai
-*📞 Telefon:* \+998 95 012 77 33
-"""
+from .constants import messages
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_markdown_v2(welcome_text.format(first_name=update.effective_user.first_name))
+    username = update.effective_user.username
+    if username == None:
+        hi = messages.get('hi')
+        msg = messages.get('username_none')
+        start_msg = f"{hi}\n\n{msg}".format(first_name=update.effective_user.first_name)
+        update.message.reply_markdown_v2(start_msg)
+    
+    else:
+        hi = messages.get('hi')
+        msg = messages.get('username_exists')
+        start_msg = f"{hi}\n\n{msg}".format(first_name=update.effective_user.first_name)
+        
+        update.message.reply_markdown_v2(start_msg)
---
+++ lms/constants.py
@@ -0,0 +1,16 @@
+messages = {
+    "hi": "Assalomu alaykum, {first_name}\!",
+    "username_none": "Sizda telegram *username* mavjud emas\! Iltimos, quyidagi video orqali username kiriting\!",
+    "username_exists": "Ro'yhatdan o'tish uchun pastdagi tugmani bosing👇👇",
+    "header": """
+Assalomu alaykum, {first_name}\!
+
+*CODEACADEMY* dasturchilar maktabi botiga xush kelibsiz\!
+
+Kelejak kasbini o'rganmoqchi bo'lsangiz bizga qo'shiling\!
+
+*📱 Telegram:* @codeacademyai
+*📞 Telefon:* \+998 95 012 77 33
+    """,
+
+}
